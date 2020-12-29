bf55a16a074a9561a6f5e0188d66573f07bab565
sujin-park <psujin831@gmail.com>
2020-12-29 21:57:01 +0900

    feature: use teleport example

---
+++ src/components/modal.vue
@@ -0,0 +1,14 @@
+<template>
+  <teleport to="#modal">
+    <div class="modal">
+      모달 제목
+      <slot />
+    </div>
+  </teleport>
+</template>
+
+<script>
+export default {
+  name: 'Modal'
+};
+</script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <modal>모달 내용</modal>
     <template v-for="item in list" :key="item.id">
       <movie-card :item="item" />
     </template>
@@ -10,11 +11,13 @@
 import { reactive, toRefs, onMounted } from 'vue';
 import { getMovies } from '@/api/movie';
 
+import Modal from '@/components/modal';
 import MovieCard from '@/components/movie-card';
 
 export default {
   name: 'Home',
   components: {
+    Modal,
     MovieCard
   },
   setup() {
